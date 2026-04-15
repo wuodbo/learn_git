442ea7667364b33b978ce98cc0bd7244cc2fed0b
wuodbo <kprotokovich@inbox.ru>
2026-04-15 16:06:48 +0300

    Добавление еще одной папки проекта

---
+++ Магазин/footer.php
@@ -0,0 +1,22 @@
+    </main>
+    <footer>
+        <p align="center">Подвал сайта</p>
+        <nav>
+            <ul>
+                <li>
+                    <a href="">Главная страница</a>
+                </li>
+                <li>
+                    <a href="">Каталог</a>
+                </li>
+                <li>
+                    <a href="">О нас</a>
+                </li>
+                <li>
+                    <a href="">Контакты</a>
+                </li>
+            </ul>
+        </nav>
+    </footer>
+</body>
+</html>
---
+++ Магазин/index.php
@@ -0,0 +1,39 @@
+<?php
+require "header.php";
+?>
+<section class="popular-tovar">
+    <h3>Популярные товары</h3>
+
+    <div class="tovar-list">
+        <div class="tovar-card">
+            <div class="tovar-img">
+                <img src="./img/keyboard.png" alt="" width="350" height="300">
+            </div>
+            <div class="tovar-info">
+                <p>Клавиатура</p>
+                <a href="./product.html">Подробнее</a>
+            </div>
+        </div>
+        <div class="tovar-card">
+            <div class="tovar-img">
+                <img src="./img/headphones.png" alt="" width="350" height="300">
+            </div>
+            <div class="tovar-info">
+                <p>Наушники</p>
+                <a href="/product2.html">Подробнее</a>
+            </div>
+        </div>
+        <div class="tovar-card">
+            <div class="tovar-img">
+                <img src="./img/mouse.png" alt="" width="350" height="300" >
+            </div>
+            <div class="tovar-info">
+                <p>Компьютерная мышь</p>
+                <a href="/product3.html">Подробнее</a>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+require "footer.php";
+?>
